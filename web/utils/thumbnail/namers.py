e718721ceeb57f7bbb117813e234079341674dff
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-26 14:20:41 +0100

    fix: Don't generate seprate hash for different resolution

--- web/utils/thumbnail/namers.py
+++ web/utils/thumbnail/namers.py
@@ -8,6 +8,6 @@ def hashed(source_filename, prepared_options, thumbnail_extension, **kwargs):
 	if output_format:
 		thumbnail_extension = f'{thumbnail_extension}.{output_format}'
 		prepared_options = [opt for opt in prepared_options if opt != f'output_format-{output_format}']
-	digest = hashlib.sha1(':'.join(prepared_options).encode('utf-8')).digest()
+	digest = hashlib.sha1(':'.join(prepared_options[1:]).encode('utf-8')).digest()
 	safe_hash = base64.urlsafe_b64encode(digest[:(len(digest)//3)*3]).decode('utf-8')
 	return '.'.join([source_filename, '_'.join(prepared_options[:1] + [safe_hash]), thumbnail_extension])
