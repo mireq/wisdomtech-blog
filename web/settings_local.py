a4d9cbe0bc086a245d09177e330fb569fda8c2ca
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-26 13:53:07 +0100

    feat: Allow enable SQL query logging

--- web/settings_local.py
+++ web/settings_local.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from .settings import *
 
 ALLOWED_HOSTS = ['*']
+LOG_SQL = False
 
 DEBUG = True
 
@@ -12,3 +13,33 @@ INSTALLED_APPS = INSTALLED_APPS + [
 ]
 
 COMPRESS_MTIME_DELAY = 0
+
+LOGGING = {
+	'version': 1,
+	'filters': {
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
+		}
+	},
+	'handlers': {
+		'console': {
+			'level': 'WARNING',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'sql': {
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler',
+		},
+	},
+	'loggers': {
+		'': {
+			'level': 'WARNING',
+			'handlers': ['console'],
+		},
+		'django.db.backends': {
+			'level': 'DEBUG' if LOG_SQL else 'WARNING',
+			'handlers': ['sql'],
+		},
+	}
+}
