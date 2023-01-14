a6415c584040fcf853454ba0c976315ff27a3400
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-14 19:06:27 +0100

    fix: Removed debug statements

--- web/utils/thumbnail/namers.py
+++ web/utils/thumbnail/namers.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import base64
 import hashlib
-from uuid import uuid4
 
 
 def hashed(source_filename, prepared_options, thumbnail_extension, **kwargs):
@@ -11,4 +10,4 @@ def hashed(source_filename, prepared_options, thumbnail_extension, **kwargs):
 		prepared_options = [opt for opt in prepared_options if opt != f'output_format-{output_format}']
 	digest = hashlib.sha1(':'.join(prepared_options[1:]).encode('utf-8')).digest()
 	safe_hash = base64.urlsafe_b64encode(digest[:(len(digest)//3)*3]).decode('utf-8')
-	return '.'.join([source_filename, '_'.join(prepared_options[:1] + [safe_hash, str(uuid4())]), thumbnail_extension])
+	return '.'.join([source_filename, '_'.join(prepared_options[:1] + [safe_hash]), thumbnail_extension])
