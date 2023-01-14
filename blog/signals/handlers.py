9d30f7e312d5add392a6cdf6d336c2b600ac2223
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-14 19:08:06 +0100

    feat: added post save signal handler

--- blog/signals/handlers.py
+++ blog/signals/handlers.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 from ..models import BlogPost
 
 
 BlogPostTranslation = BlogPost._parler_meta.root_model
+
+
+@receiver(pre_save, sender=BlogPostTranslation)
+def blog_post_pre_save(sender, instance: BlogPostTranslation, **kwargs): # pylint: disable=unused-argument
+	print(instance)
