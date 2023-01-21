3bfa715f0c2a174832ace57ca24a140518e28777
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-21 15:30:55 +0100

    feat: Generate article image thumbnail without template

--- web/utils/lxml_utils.py
+++ web/utils/lxml_utils.py
@@ -7,11 +7,11 @@ import lxml.html
 from django.conf import settings
 from django.core.files.storage import default_storage
 from django.db.models.fields.files import FieldFile
-from django.template.loader import render_to_string
 from django_attachments.models import Attachment
 from lxml import etree
 
 from web.utils.syntax_highlight import format_code
+from web.utils.thumbnail.generator import generate_thumbnails
 
 
 logger = logging.getLogger(__name__)
@@ -74,9 +74,39 @@ def make_thumbnails(element):
 			field = Attachment._meta.get_field('file')
 			field_file = FieldFile(instance=None, field=field, name=image_path)
 			field_file.file = fp
-			ctx = {'image': field_file}
-			html_content = render_to_string('blog/partials/article_image.html', ctx)
-			return html_content
+
+			thumbnails = generate_thumbnails(field_file, 'article', sizes=['328px', '496px', '656px', '1044px', '1392px'], size_attrs={'quality': [60, 60, 60, 50, 40]})
+			if not thumbnails:
+				return element
+
+			img_attrs = dict(element.attrib)
+			first = thumbnails[0]
+
+			img_attrs['width'] = str(first.size[0])
+			img_attrs['height'] = str(first.size[1])
+			img_attrs['src'] = str(first.url)
+
+			img_srcs = []
+			webp_srcs = []
+
+			for thumbnail in thumbnails:
+				if thumbnail.format == 'webp':
+					webp_srcs.append(f'{thumbnail.url} {thumbnail.size_hint}')
+				else:
+					img_srcs.append(f'{thumbnail.url} {thumbnail.size_hint}')
+
+			webp_srcs = ', '.join(webp_srcs)
+			img_srcs = ', '.join(img_srcs)
+			if img_srcs:
+				img_attrs['srcset'] = img_srcs
+
+			if webp_srcs:
+				picture = lxml.etree.Element('picture')
+				lxml.etree.SubElement(picture, 'source', {'type': 'image/webp', 'srcset': webp_srcs})
+				lxml.etree.SubElement(picture, 'img', img_attrs)
+				return picture
+			else:
+				return lxml.etree.Element('img', img_attrs)
 	except Exception:
 		logger.exception("Failed to generate thumbnails")
 		return element
