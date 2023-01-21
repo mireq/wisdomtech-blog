72a2e5c498e72fdea37e99d1ec48a40a832faf7e
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-21 12:49:55 +0100

    feat: Added management command to preprocess content

---
+++ blog/management/commands/blog_management.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.core.management import BaseCommand, CommandParser
+
+from ...models import BlogPost
+from web.utils.content import process_content
+
+
+BlogPostTranslation = BlogPost._parler_meta.root_model
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		cmd = self
+
+		class SubParser(CommandParser):
+			def __init__(self, **kwargs):
+				kwargs['called_from_command_line'] = getattr(cmd, "_called_from_command_line", None)
+				super().__init__(**kwargs)
+
+		subparsers = parser.add_subparsers(help="Command", dest='subcommand', required=True, parser_class=SubParser)
+		subparsers.add_parser('preprocess_content', help="Preprocess content")
+
+	def handle(self, *args, **options):
+		subcommand = options['subcommand']
+		return getattr(self, subcommand)(*args, **options)
+
+	def preprocess_content(self, *args, **kwargs):
+		for trans in BlogPostTranslation.objects.all():
+			old_content = trans.processed_content
+			new_content = process_content(trans.content)
+			if old_content != new_content:
+				BlogPostTranslation.objects.filter(pk=trans.pk).update(processed_content=new_content)
