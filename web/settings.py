f4bd2066844bca57e9a1cd65eb7ce2f67efd0e13
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-22 15:38:12 +0100

    feat: Display pre properties in wysiwyg editor

--- web/settings.py
+++ web/settings.py
@@ -358,16 +358,18 @@ TINYMCE_CONFIGS = {
 		'toolbar': 'undo redo | bold italic | bullist numlist outdent indent | removeformat | help | code',
 	},
 	'content': {
-		'plugins': 'advlist autolink link image lists charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking table emoticons template paste language help',
+		'plugins': 'advlist autolink link image lists charmap print preview hr anchor pagebreak searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking table emoticons template paste help',
 		'toolbar': 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullscreen | forecolor backcolor emoticons | help | code',
 		'menubar': True,
 		'style_formats': [
-			{'title': pgettext_lazy('style', "Source code"), 'items': [
-				{'title': name, 'block': 'pre', 'classes': [f'code-{code}']}
-				for code, name in SOURCE_CODE_LEXERS
-			]},
+			{'title': pgettext_lazy('style', "Source code"), 'items':
+				[{'title': pgettext_lazy('style', "Don't wrap code"), 'format': 'pre_no_wrap'}] +
+				[
+					{'title': name, 'block': 'pre', 'classes': [f'code-{code}']}
+					for code, name in SOURCE_CODE_LEXERS
+				]
+			},
 			{'title': pgettext_lazy('style', "Wide"), 'format': 'wide'},
-			{'title': pgettext_lazy('style', "Don't wrap code"), 'format': 'pre_no_wrap'},
 		],
 		'formats': {
 			'alignleft': [
