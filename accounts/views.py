726e8c47c94ddd13c0d37d6a5cbb95ec3f2a7e74
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-02-10 17:33:02 +0100

    feat: Display post list in user detail

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .models import User
+from blog.models import BlogPost
 from web.utils.generic_views import AttachmentListAndUploadView, ListView, DetailView
 
 
@@ -23,6 +24,13 @@ class UserDetailView(DetailView):
 			.fast_translate()
 			.select_related('gallery', 'gallery__primary_attachment')
 		)
+
+	def get_context_data(self, **kwargs):
+		ctx = super().get_context_data(**kwargs)
+		obj = ctx['object']
+		last_posts = BlogPost.objects.published().filter(author=obj).fast_translate(fields=['title', 'slug']).only('pk')
+		ctx['last_posts'] = last_posts
+		return ctx
 
 
 class UserAttachmentsList(AttachmentListAndUploadView):
